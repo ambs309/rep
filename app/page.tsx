a7b4cd7a6c9189ecb4c81534e689dc4170adb556
André Sobreira - a22305703 <160252318+ambs309@users.noreply.github.com>
2024-12-06 22:11:49 +0000

    message s

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+ return<>
+
+ <h1>Margem Sul</h1>
+ <p>Esta página será dedicada à apresentação de conteúdos que enaltecem o melhor sítio do mundo!</p>
+ </>
+}
